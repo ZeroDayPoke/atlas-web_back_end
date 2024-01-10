3ad5543cb3ac8878bb98c0d5a9fbf35e3846960c
Chris Stamper <cstampok@gmail.com>
2024-01-10 17:15:40 -0600

    checker test

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# ./0-add.py
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return the sum.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
