d5c74795f8acf6ba8bd2114ed94670e34b2e9ce6
Chris Stamper <cstampok@gmail.com>
2024-01-10 19:13:24 -0600

    lol aight

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -36,7 +36,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Get hypermedia information for a specific index.
 
         Args:
