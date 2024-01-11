137c5f91d5057c44981e37c1e997f8b7948b4b70
Chris Stamper <cstampok@gmail.com>
2024-01-10 18:39:28 -0600

    test docs

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -4,13 +4,40 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFOCache class"""
+    """
+    FIFOCache class that inherits from BaseCaching
+    and implements a FIFO caching system.
+
+    This caching system evicts the least recently
+    added item once it exceeds its
+    maximum capacity.
+    """
 
     def __init__(self):
+        """
+        Initialize the FIFOCache.
+
+        Overrides the parent class __init__ method.
+        Initializes the cache order list
+        which tracks the order in which items were added to the cache.
+        """
         super().__init__()
         self.cache_order = []
 
     def put(self, key, item):
+        """
+        Add an item to the cache.
+
+        If the cache exceeds its maximum capacity as
+        defined by BaseCaching.MAX_ITEMS,
+        the least recently added item is removed first.
+        If the key or item is None,
+        nothing is added to the cache.
+
+        Args:
+            key: The key under which the item will be stored.
+            item: The item to be stored in the cache.
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.cache_order:
@@ -21,6 +48,17 @@ class FIFOCache(BaseCaching):
                 print("DISCARD: {}".format(discarded))
 
     def get(self, key):
-        """ Get an item by key
+        """
+        Retrieve an item by key.
+
+        Returns the value stored in the cache.
+        If the key is None or the key does not
+        exist in the cache, None is returned.
+
+        Args:
+            key: The key of the item to be retrieved.
+
+        Returns:
+            The value stored in the cache or None if not found.
         """
         return self.cache_data.get(key, None)
