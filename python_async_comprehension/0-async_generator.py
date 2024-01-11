2664d0234bf07bf79c88853670060133b7a6f335
Chris Stamper <cstampok@gmail.com>
2024-01-10 18:09:13 -0600

    add annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,9 @@ async def async_generator():
     """
     An asynchronous generator that yields 10 random numbers,
     each after waiting 1 second.
+
+    Yields:
+    float: A random number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
