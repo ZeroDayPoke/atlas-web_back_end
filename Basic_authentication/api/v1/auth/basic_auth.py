4848669b2b9ffd267ad01b69517452c2133bbb7e
Chris Stamper <cstampok@gmail.com>
2024-01-12 15:31:26 -0600

    basic auth test success

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,11 @@ from flask import request
 
 class BasicAuth(Auth):
     """basic auth class"""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header."""
+        if authorization_header is None or \
+           not isinstance(authorization_header, str) or \
+           not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
