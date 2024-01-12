ed0348fdd73c062038bd74a8d17dda9411b8b716
Chris Stamper <cstampok@gmail.com>
2024-01-12 14:24:16 -0600

    password validation

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Hash a password for storing."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
