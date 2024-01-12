ad6d9b5dee972f42554bfc93957314bc0e7ca784
Chris Stamper <cstampok@gmail.com>
2024-01-12 15:49:55 -0600

    my love for flask login has risen lol

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,29 @@ class BasicAuth(Auth):
                 return None
         except Exception:
             return None
+
+    def current_user(self, request=None) -> UserType:
+        """Retrieves the User instance for a request."""
+        if request is None:
+            return None
+
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        if base64_auth_header is None:
+            return None
+
+        decoded_base64_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
+        if decoded_base64_auth_header is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64_auth_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        return self.user_object_from_credentials(user_email, user_pwd)
