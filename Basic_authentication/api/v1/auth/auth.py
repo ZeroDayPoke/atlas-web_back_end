1dcbb255ad991d9c9d6f3cee73ed6be8e5c517c0
Chris Stamper <cstampok@gmail.com>
2024-01-12 15:04:42 -0600

    simple enough

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Auth class for the API."""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Determines if authentication is required for a given path."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Retrieves the Authorization header from a Flask request object."""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the current user from a Flask request object."""
+        return None
