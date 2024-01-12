ea616b5e327df48a0a756c14dab6fbb82fff996b
Chris Stamper <cstampok@gmail.com>
2024-01-12 15:07:15 -0600

    auth check

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,12 @@ class Auth:
     """auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required for a given path."""
-        return False
+        if path is None or not excluded_paths:
+            return True
+
+        path = path + '/' if not path.endswith('/') else path
+
+        return not any(ep == path for ep in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """Retrieves the Authorization header from a Flask request object."""
