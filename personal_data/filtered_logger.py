ea70ef031878040f5a572544ddcd225db429275b
Chris Stamper <cstampok@gmail.com>
2024-01-12 13:43:12 -0600

    styling

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,10 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    regex_pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
+    regex_pattern = '|'.join([f'(?<={field}=)[^{separator}]+'
+                              for field in fields])
     return re.sub(regex_pattern, redaction, message)
