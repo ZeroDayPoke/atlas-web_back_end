ae06444ef7aa8ac7c9908e456a31795869bd1b17
Chris Stamper <cstampok@gmail.com>
2024-01-12 13:57:02 -0600

    docs

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -20,10 +20,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """initializes RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Filters values in incoming log records using filter_datum"""
         original_message = super().format(record)
         return filter_datum(self.fields, self.REDACTION,
                             original_message, self.SEPARATOR)
