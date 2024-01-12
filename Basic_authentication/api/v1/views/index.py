5f681fc34018404cbc10cdf22961fb81a8520905
Chris Stamper <cstampok@gmail.com>
2024-01-12 14:53:53 -0600

    working fine locally

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
-def unauthorized():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
   """unauthorized route"""
   abort(401)
