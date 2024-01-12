6b18a53127a0b76694275196e30ac1d9e65d5639
Chris Stamper <cstampok@gmail.com>
2024-01-12 13:40:32 -0600

    anootation required

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Logger"""
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    regex_pattern = '|'.join([f'(?<={field}=)[^;]+' for field in fields])
+    regex_pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
     return re.sub(regex_pattern, redaction, message)
