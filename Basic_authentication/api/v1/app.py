a60caac9e8e1f3c7d3590715ffd2e72df14a125b
Chris Stamper <cstampok@gmail.com>
2024-01-12 15:13:07 -0600

    solid auth test

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -2,9 +2,9 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify
-from werkzeug.exceptions import Unauthorized
+from flask import Flask, jsonify, abort, request
 from os import getenv
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -14,6 +14,25 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+auth = Auth() if AUTH_TYPE == "auth" else None
+
+
+@app.before_request
+def before_request_auth():
+    """Filter each request before processing."""
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -16,8 +16,10 @@ class Auth:
         return not any(ep == path for ep in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
-        """Retrieves the Authorization header from a Flask request object."""
-        return None
+        """Retrieve the Authorization header."""
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Retrieves the current user from a Flask request object."""
