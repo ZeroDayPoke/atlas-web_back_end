1c6d567d0bbe99a85659e2c31ec71700d6c10012
Chris Stamper <cstampok@gmail.com>
2024-01-19 18:44:14 -0600

    solid test

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 """auth module"""
 import bcrypt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """
-    Hash a password for storing.
-    """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a user with the provided email and password.
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {email} already exists")
+
+        hashed_password = self._hash_password(password)
+        new_user = self._db.add_user(email, hashed_password)
+        return new_user
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hash a password for storing.
+        """
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -37,13 +37,8 @@ class DB:
         """
         Finds a user by arbitrary keyword arguments.
         """
-        try:
-            query = self._session.query(User).filter_by(**kwargs).first()
-            if query is None:
-                raise NoResultFound
-            return query
-        except InvalidRequestError:
-            raise
+        result = self._session.query(User).filter_by(**kwargs).first()
+        return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
