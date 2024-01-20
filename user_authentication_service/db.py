f7b14d167091aa089cca552fcb5a18c56f8c3927
Chris Stamper <cstampok@gmail.com>
2024-01-19 18:34:44 -0600

    anno

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
         Finds a user by arbitrary keyword arguments.
         """
