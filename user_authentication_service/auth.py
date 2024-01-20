7dec67e2af3ef26a72afe8ce069dca24a9a35db5
Chris Stamper <cstampok@gmail.com>
2024-01-19 19:02:49 -0600

    auth

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -54,6 +54,14 @@ class Auth:
             return session_id
         return None
 
+    def get_user_from_session_id(self, session_id: str):
+        """
+        Get a user from a session ID.
+        """
+        if session_id is None:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        return user
 
 def _hash_password(self, password: str) -> bytes:
     """
