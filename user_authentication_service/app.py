f6ec38d4b5c335fda44a162511d6f5a5d5bc8b11
Chris Stamper <cstampok@gmail.com>
2024-01-19 18:51:05 -0600

    style

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,11 +2,14 @@
 """app module"""
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
