3e6e499bdc031afe51dafa8f75bf9bdc0e5ecf21
Chris Stamper <cstampok@gmail.com>
2024-01-19 18:35:59 -0600

    test success

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -44,3 +44,16 @@ class DB:
             return query
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs):
+        """
+        Update a user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
