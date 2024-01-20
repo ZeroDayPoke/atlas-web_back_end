7d25d04c85e614f08ac562ef9054cd2f9cc80a10
Chris Stamper <cstampok@gmail.com>
2024-01-19 19:15:31 -0600

    update pass

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -87,6 +87,19 @@ class Auth:
 
         return reset_token
 
+    def update_password(self, reset_token: str, password: str):
+        """
+        Update a user's password.
+        """
+        user = self._db.find_user_by(reset_token=reset_token)
+        if user is None:
+            raise ValueError("Reset token is invalid")
+
+        hashed_password = self._hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_password,
+                             reset_token=None)
+
 
 def _hash_password(self, password: str) -> bytes:
     """
