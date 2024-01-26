ca76be90a89903c7e2b8ad4362c76f470f131773
Chris Stamper <cstampok@gmail.com>
2024-01-26 17:10:55 -0600

    solid test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -2,8 +2,17 @@
 """Unittests for client.py"""
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+import fixtures
+
+
+class MockResponse:
+    """Mock class for the response object"""
+    def __init__(self, json_data):
+        self.json_data = json_data
+    def json(self):
+        return self.json_data
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -61,3 +70,40 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient('test_org')
         result = github_org_client.has_license(repo, license_key)
         self.assertEqual(result, expected_result)
+
+
+@parameterized_class([
+    {"org_payload": payload[0], "repos_payload": payload[1], "expected_repos": payload[2], "apache2_repos": payload[3]}
+    for payload in fixtures.TEST_PAYLOAD
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the tests by mocking requests.get."""
+        def side_effect(url):
+            """Helper function for side_effect to return the correct fixture."""
+            response = None
+            if url.startswith("https://api.github.com/orgs/"):
+                response = MockResponse(cls.org_payload)
+            if url.endswith("/repos"):
+                response = MockResponse(cls.repos_payload)
+            if not response:
+                response = MockResponse({})
+    
+            print(f"URL: {url}, Response type: {type(response.json_data)}")
+            return response
+
+        cls.get_patcher = patch('requests.get', side_effect=side_effect)
+        cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear down the tests by stopping the patcher."""
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Test public repos with mocked data."""
+        test_client = GithubOrgClient('test_org')
+        repos = test_client.public_repos()
+        self.assertEqual(repos, self.expected_repos)
