7aebe7741e322a73e3871fda3827f3623c5c2a20
Chris Stamper <cstampok@gmail.com>
2024-01-26 17:02:07 -0600

    solid test

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -30,3 +30,18 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = mock_payload
             github_org_client = GithubOrgClient('org')
             self.assertEqual(github_org_client._public_repos_url, 'https://api.github.com/orgs/org/repos')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test GithubOrgClient.public_repos."""
+        mock_repo_url = 'https://api.github.com/orgs/org/repos'
+        mock_repos_payload = [{'name': 'repo1'}, {'name': 'repo2'}]
+        mock_get_json.return_value = mock_repos_payload
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = mock_repo_url
+            github_org_client = GithubOrgClient('org')
+            repos = github_org_client.public_repos()
+
+            self.assertEqual(repos, [repo['name'] for repo in mock_repos_payload])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(mock_repo_url)
