54efdd6ef8aec8f8b5febe6db5c185476093f016
Chris Stamper <cstampok@gmail.com>
2024-01-26 18:06:17 -0600

    test

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,20 +1,24 @@
-
 #!/usr/bin/env python3
-
+"""flask app"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
+    """config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
+    """get_locale"""
     # Check if request has locale parameter and if it's supported
     requested_locale = request.args.get('locale')
     if requested_locale in app.config['LANGUAGES']:
@@ -22,9 +26,12 @@ def get_locale():
     # Default behavior
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
+    """index"""
     return render_template('4-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
